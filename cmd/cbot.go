e5b07192c0aa2f6a6fe17d556820f0dc9c88695a
bohdan <bohdan@test.com>
2025-06-01 20:22:25 +0000

    basik implentation

--- cmd/cbot.go
+++ cmd/cbot.go
@@ -1,5 +1,6 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
@@ -9,11 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var appVersion = "Version"
-
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
+// cbotCmd represents the cbot command
+var cbotCmd = &cobra.Command{
+	Use:   "cbot",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -22,20 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(appVersion)
+		fmt.Println("cbot called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(cbotCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// cbotCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// cbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

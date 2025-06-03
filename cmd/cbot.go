4bf65de7caa1cdaf563d9624b539ae9a66682692
bohdan <bohdan@test.com>
2025-06-03 17:56:36 +0000

    bot basik implementation

--- cmd/cbot.go
+++ cmd/cbot.go
@@ -5,35 +5,106 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/stianeikeland/go-rpio"
+	"gopkg.in/telebot.v4"
 )
+
+var (
+	// TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+type TrafficSignal struct {
+	Pin int8
+	On  bool
+}
 
 // cbotCmd represents the cbot command
 var cbotCmd = &cobra.Command{
-	Use:   "cbot",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:     "cbot",
+	Aliases: []string{"start"},
+	Short:   "Telegram bot for controlling traffic light signals",
+	Long: `A Telegram bot that allows controlling traffic light signals through GPIO pins.
+The bot accepts commands to toggle red, amber, and green lights on/off.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Usage:
+  /s red    - Toggle red light
+  /s amber  - Toggle amber light
+  /s green  - Toggle green light
+  hello     - Get a greeting from the bot`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cbot called")
+		if TeleToken == "" {
+			log.Fatal("TELE_TOKEN environment variable is not set")
+		}
+
+		fmt.Printf("cbot %s started\n", appVersion)
+
+		cbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+		}
+
+		if err := rpio.Open(); err != nil {
+			log.Fatalf("Unable to open GPIO: %s", err)
+		}
+		defer rpio.Close()
+
+		// Initialize traffic signals
+		trafficSignals := map[string]*TrafficSignal{
+			"red":   {Pin: 12, On: false},
+			"amber": {Pin: 27, On: false},
+			"green": {Pin: 22, On: false},
+		}
+
+		// Initialize all pins as input
+		for _, signal := range trafficSignals {
+			pin := rpio.Pin(signal.Pin)
+			pin.Input()
+		}
+
+		cbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Printf("Received message: %s", m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				return m.Send(fmt.Sprintf("Hello I'm cbot %s!", appVersion))
+
+			case "red", "amber", "green":
+				signal := trafficSignals[payload]
+				pin := rpio.Pin(signal.Pin)
+
+				if !signal.On {
+					pin.Output()
+					pin.High()
+					signal.On = true
+				} else {
+					pin.Low()
+					pin.Input()
+					signal.On = false
+				}
+
+				return m.Send(fmt.Sprintf("Switched %s light %s", payload, map[bool]string{true: "on", false: "off"}[signal.On]))
+
+			default:
+				return m.Send("Usage: /s red|amber|green")
+			}
+		})
+
+		cbot.Start()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(cbotCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cbotCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/dominusalpha/cbot
 
 go 1.24.2
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/spf13/cobra v1.9.1
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible
+	gopkg.in/telebot.v4 v4.0.0-beta.5
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
